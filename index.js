bc538a07202e81cbebb04b789a128641bd7122b7
rabi27 <rabisiddique27@gmail.com>
2023-03-04 14:06:23 +0500

    initialize redis client

---
+++ index.js
@@ -0,0 +1,13 @@
+const redis = require('redis');
+const client = redis.createClient({
+  port: process.env.REDIS_PORT || 6379,
+  host: process.env.REDIS_HOST || 'localhost',
+});
+
+client.connect();
+
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+client.on('connect', function () {
+  console.log('Connected to Redis');
+});
